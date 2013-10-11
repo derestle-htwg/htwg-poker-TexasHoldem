52bf195009429e707f2a0b2640531d25e30994b3
dennis.restle <derestle@htwg-konstanz.de>
2013-10-11 20:04:22 +0200

    Tui start hinzugefügt

--- htwg-poker-TexasHoldem/src/de/htwg/se/poker/view/Tui.java
+++ htwg-poker-TexasHoldem/src/de/htwg/se/poker/view/Tui.java
@@ -1,5 +1,67 @@
 package de.htwg.se.poker.view;
 
-public class Tui implements GameInterface{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.List;
 
+import de.htwg.se.poker.controller.*;
+
+public class Tui implements GameInterface{
+	
+	private InputStream DataInput;
+	private PrintStream DataOutput;
+	
+	BufferedReader br;
+	
+	public Tui()
+	{
+		this(System.in,System.out);
+	}
+	
+	public Tui(InputStream inStr, PrintStream outStr)
+	{
+		DataInput = inStr;
+		DataOutput = outStr;
+		br= = new BufferedReader(new InputStreamReader(DataInput));
+	}
+	
+	
+
+	public List<Player> getNewPlayers()
+	{
+		List<Player> players = new LinkedList<Player>();
+		
+		do{
+			Player tmpPly = new Player();
+			DataOutput.println("Name des " + players.size() + ". Spielers: ");
+			
+			tmpPly.name = readLine();
+			players.add(tmpPly);
+			
+			DataOutput.println("\nZusätzlichen Spieler? [j/n]");
+			
+		}while(readLine().equalsIgnoreCase("j"));
+
+		return players;
+	}
+	
+	public int readInt()
+	{
+		return Integer.parseInt(readLine());
+	}
+	
+	public String readLine()
+	{
+		try {
+			return br.readLine();
+		} catch (IOException ioe) {
+		System.out.println("IO error!");
+		System.exit(1);}
+		return null;
+	}
 }
