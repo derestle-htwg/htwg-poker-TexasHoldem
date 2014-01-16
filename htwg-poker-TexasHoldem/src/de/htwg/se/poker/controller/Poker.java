707e6507a40957c7603032f7eb73fc96bc0b8c62
dennis <dennis@dennis-ThinkPad-R60>
2014-01-16 22:44:23 +0100

    Erste Änderungen am StartCode für Interfaceauswahl und Spielerkonfiguration

--- htwg-poker-TexasHoldem/src/de/htwg/se/poker/controller/Poker.java
+++ htwg-poker-TexasHoldem/src/de/htwg/se/poker/controller/Poker.java
@@ -1,21 +1,113 @@
 package de.htwg.se.poker.controller;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
 import de.htwg.se.poker.model.*;
 import de.htwg.se.poker.model.Card.Rank;
 import de.htwg.se.poker.model.Card.Suit;
+import de.htwg.se.poker.view.PlayerInterface;
+import de.htwg.se.poker.view.PlayerTui;
 
 public class Poker {
-		
+	
+	static int MIN_PLAYERS = 2;
+	static int MAX_PLAYERS = 8;
+	static double STARTCAPITAL = 100.0;
+	/*Was wird für ein Spiel benötigt?
+	 * Anzahl Spieler
+	 * Interfaces d. Spieler
+	 * */
+	
 	public static void main(String[] args) 
 	{
-		Table table = new Table(null);
+		List<Class<? extends PlayerInterface>> interfaces = getAllInterfaces();
+		List<Player> Players = new LinkedList<Player>();
 		
-		//table.tableElem.getCardsElements().setMiddleCards();
 		
-		int round = 0;
-		round++;
+		int PlayerCount = askPlayerCount();
 		
-		//table.setTableComponents(round);
+		for(int i = 0; i<PlayerCount;i++)
+		{
+			PlayerInterface pi = askPlayerInterface(interfaces,i+1);
+			Player p = new Player(pi.getPlayerName(),STARTCAPITAL);
+			Players.add(p);
+		}
+		GameMaster myGame = new GameMaster();
+		
+		
+	}
+	
+	private static PlayerInterface askPlayerInterface(List<Class<? extends PlayerInterface>> myInterfaces, int PlayerNo)
+	{
+		System.out.println("Was für ein Interface benutzt Spieler " + PlayerNo + "?");
+		
+		HashMap<Integer, Class<? extends PlayerInterface>> Choices = new HashMap<Integer, Class<? extends PlayerInterface>>(); 
+		
+		int i = 0;
+		for(Class<? extends PlayerInterface> c : myInterfaces)
+		{
+			i++;
+			System.out.println("[" + i + "] " + c.getSimpleName());
+			Choices.put(i, c);
+		}
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int InterfaceNr = 0;
+		while(InterfaceNr < 1 || InterfaceNr > i)
+		{
+			String text = null;
+			try {
+				text = br.readLine();
+				InterfaceNr = Integer.parseInt(text);
+			} catch (NumberFormatException e) {
+				System.out.println("\"" + text + "\"ist kein gültiges Interface!");
+			} catch (IOException e) {
+				System.out.println("Fehler bei der Eingabe,");			
+			}
+		}
+		PlayerInterface retVal = null;
+		
+		try {
+			retVal = Choices.get(InterfaceNr).newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		return retVal;
+	}
+
+	private static int askPlayerCount() {
+		System.out.println("Anzhal der Spieler:");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int Players = 0;
+		while(Players < MIN_PLAYERS || Players > MAX_PLAYERS)
+		{
+			String text = "";
+			try {
+				text = br.readLine();
+				Players = Integer.parseInt(text);
+			} catch (NumberFormatException e) {
+				System.out.println("\"" + text + "\"ist keine gültige Anzahl spieler!");
+				System.out.println("Es müssen mindestens 2 und dürfen höchstens 8 Spieler teilnehmen.");
+			} catch (IOException e) {
+				System.out.println("Fehler bei der Eingabe,");			
+			}
+		}
+		return Players;
+	}
+	
+	private static List<Class<? extends PlayerInterface>> getAllInterfaces()
+	{
+		LinkedList<Class<? extends PlayerInterface>> myInterfaces = new LinkedList<Class<? extends PlayerInterface>>();
+		myInterfaces.add(PlayerTui.class);
+		return myInterfaces;
 	}
 		
 }
